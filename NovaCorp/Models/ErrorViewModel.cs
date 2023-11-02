cb3edc51cc7921b2cf1b5e03183b42abf0de4227
GUIDO COHEN FALAH <47755313@ort>
2023-11-02 14:33:25 -0300

    segundo commit

---
+++ NovaCorp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NovaCorp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Proyecto NovaCorp hecho por: Matías Esteves, Franco Ragusa y Guido Cohen Falah
+
